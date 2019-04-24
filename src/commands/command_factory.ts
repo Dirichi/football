d8059e8f048fd002ca17844e30a45b82056de9a5
Dirichi <din206@nyu.edu>
2019-04-24 20:34:47 +0100

    Add TODO on CommandFactory

--- src/commands/command_factory.ts
+++ src/commands/command_factory.ts
@@ -1,16 +1,9 @@
 import { ICommand } from "../interfaces/icommand";
 import { ICommandFactory } from "../interfaces/icommand_factory";
-import { ChaseBallCommand } from "./chase_ball_command";
-import { MoveDownCommand } from "./move_down_command";
-import { MoveLeftCommand } from "./move_left_command";
-import { MoveRightCommand } from "./move_right_command";
-import { MoveToAttackingPositionCommand } from "./move_to_attacking_position_command";
-import { MoveToDefensivePositionCommand } from "./move_to_defensive_position_command";
-import { MoveUpCommand } from "./move_up_command";
-import { PassBallCommand } from "./pass_ball_command";
-import { ShootBallCommand } from "./shoot_ball_command";
 
 export class CommandFactory implements ICommandFactory {
+  // TODO: Remove this class and pass the mapping directly to its clients
+  // or return an error in `getCommand` or a `NullCommand` object
   private nameToCommandMapping: Map<string, ICommand>;
 
   constructor(nameToCommandMapping: Map<string, ICommand>) {
