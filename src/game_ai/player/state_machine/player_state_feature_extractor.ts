4368dbd8f4fcbce58413f2cd2621e428399fe8fa
Dirichi <din206@nyu.edu>
2019-04-26 02:15:22 +0100

    Add ballPossessionService to feature extractor

--- src/game_ai/player/state_machine/player_state_feature_extractor.ts
+++ src/game_ai/player/state_machine/player_state_feature_extractor.ts
@@ -1,4 +1,5 @@
 import { Ball } from "../../../game_objects/ball";
+import { BallPossesionService } from "../../../services/ball_possession_servvice";
 import { IPlayerStateFeatureExtractor } from "../../../interfaces/iplayer_state_feature_extractor";
 import { minimumBy } from "../../../utils/helper_functions";
 import { Player } from "../../../game_objects/player";
@@ -7,8 +8,9 @@ export class PlayerStateFeatureExtractor implements IPlayerStateFeatureExtractor
   // TODO: Test this class
   private ball: Ball;
 
-  constructor(ball: Ball) {
+  constructor(ball: Ball, ballPossessionService: BallPossesionService) {
     this.ball = ball;
+    this.ballPossessionService = ballPossessionService;
   }
 
   public teamInControl(player: Player): boolean {
