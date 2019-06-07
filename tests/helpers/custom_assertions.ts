2396417420dd31df8dc311b36d9b2d1680f0e929
Dirichi <din206@nyu.edu>
2019-06-07 21:02:39 +0100

    Remove unused imports

--- tests/helpers/custom_assertions.ts
+++ tests/helpers/custom_assertions.ts
@@ -1,6 +1,5 @@
 import * as chai from 'chai';
 import * as sinon from 'sinon';
-import { ThreeDimensionalVector } from "../../src/three_dimensional_vector";
 
 const sinonChai = require('sinon-chai');
 const expect = chai.expect;
