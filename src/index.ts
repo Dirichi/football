3e965b271190e1bf8b50ed0f6e9129643988b7aa
Dirichi <din206@nyu.edu>
2019-06-11 23:04:06 +0100

    WIP: Trying to identify socket

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import cookieSession from "cookie-session";
 import express from "express";
 import * as http from "http";
 import path from "path";
 import socketIo from "socket.io";
+import v4 from "uuid/v4";
 import { GAME_EXECUTABLE_FILE } from "./constants";
 import { EventQueue } from "./event_queue";
 import { GameClient } from "./game_client";
@@ -13,14 +15,21 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = socketIo(httpServer);
 const port = 3000;
-// TODO: Create a game room on authenticated user request;
+
+const cookieSessionOptions = {
+  name: "session",
+  secret: "CHANGE THIS SECRET",
+};
 
 const forker = new WrappedProcessForker();
-
 const room = new GameRoom();
 room.setProcessForker(forker);
 room.setGameExecutablePath(path.join(__dirname, GAME_EXECUTABLE_FILE));
 room.save();
+
+// TODO: Figure out what this means
+app.set("trust proxy", 1); // Trust first proxy.
+app.use(cookieSession(cookieSessionOptions));
 
 // Configure Express to use EJS
 app.set("views", path.join(__dirname, "views"));
@@ -29,7 +38,40 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
+  if (!req.session.playerId) {
+    req.session.playerId = v4();
+  }
   res.render("index");
+});
+
+app.post("/games", (req, res) => {
+  if (!req.session.playerId) {
+    res.render("sign_in");
+    return;
+  }
+
+  // const room = new GameRoom();
+  // room.setProcessForker(forker);
+  // room.setGameExecutablePath(path.join(__dirname, GAME_EXECUTABLE_FILE));
+  // room.save();
+
+  // res.redirect(`/games/${room.getId()}`);
+  res.redirect(`/games/1`);
+});
+
+app.get("/games/:id", (req, res) => {
+  if (!req.session.playerId) {
+    res.render("sign_in");
+    return;
+  }
+
+  // const roomId = req.params.id;
+  // const room = GameRoom.find(roomId);
+  // if (room) {
+  //   res.render("game", { gameId: room.getId() });
+  // }
+
+  res.render("game");
 });
 
 // start the Express server
@@ -37,17 +79,18 @@ httpServer.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`server started at http://localhost:${port}`);
 });
-let clientSize = 0;
 
 io.on("connection", (socket) => {
   const wrappedSocket = new WrappedSocket(socket);
   const client = new GameClient(wrappedSocket);
-  // client.configure();
-  // TODO: Hacky way to implement room size limit / timeout. Will be replaced.
-  clientSize += 1;
-  setTimeout(() => { room.startGame(); }, 3000);
+  // tslint:disable-next-line:no-console
+  console.log(socket.handshake.headers.cookie);
+  // const id = client.requestPlayerId();
+  // const roomId = client.requestRoomId();
+  // const room = GameRoom.find(roomId);
+  // if (room && room.expectingClient(client)) {
+  //   room.addClient(client);
+  // }
+  setTimeout(() => { room.startGame(); }, 1000);
   room.addClient(client);
-  if (clientSize === 2) {
-    room.startGame();
-  }
 });
