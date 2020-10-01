21c20676ac3ffa2a0e095c0d813a9db32c3ba3d4
ike-njoku <ikenjokudc@gmail.com>
2020-10-01 00:56:38 -0700

    added login form component into app component

--- login/src/app/app.module.ts
+++ login/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
