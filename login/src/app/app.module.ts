e9c150612808a9a3bc3a153392c95c6fc57feea1
ike-njoku <ikenjokudc@gmail.com>
2020-10-02 13:12:00 -0700

    added tips component (boiler plate)

--- login/src/app/app.module.ts
+++ login/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { BackgroundComponent } from './background/background.component';
+import { TipsComponent } from './tips/tips.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    BackgroundComponent,
+    TipsComponent
   ],
   imports: [
     BrowserModule
--- login/src/app/login-form/login-form.component.ts
+++ login/src/app/login-form/login-form.component.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  // position selected on click of the view
+  position;
+
+  // playable positions
+  positions = [
+    'striker',
+    'midfielder',
+    'defender'
+  ];
+
+  // select position
+  select(position){
+    // select the position
+    this.position = position;
+    window.alert(position);
+  }
 
   constructor() { }
 
