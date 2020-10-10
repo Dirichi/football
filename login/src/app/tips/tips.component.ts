093e853593697ba3583c4d9c760178a1a179b458
ike-njoku <ikenjokudc@gmail.com>
2020-10-09 22:49:21 -0700

    added interface for tips and rules

--- login/src/app/tips/tips.component.ts
+++ login/src/app/tips/tips.component.ts
@@ -1,62 +1,97 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { Console } from 'console';
 @Component({
   selector: 'app-tips',
   templateUrl: './tips.component.html',
   styleUrls: ['./tips.component.css']
 })
+
 export class TipsComponent implements OnInit {
 
 
   // hard coded tips
   tips = [
-    'Use direction (Arrow) keys to move.',
-    'Press A to shoot.',
-    'Press S to pass.',
-    'Press D to request a pass. (only works on non-player characters)',
-    'Press F to chase after the ball.'
+    'Use direction (Arrow Keys) to move...',
+    'Press A to shoot...',
+    'Press S to pass...',
+    'Press D to request a pass. (only works on non-player characters)...',
+    'Press F to chase after the ball...'
   ];
 
-  // current tip Index
+
+
+  // get the current tipIndes
   currentTipIndex = 0;
-  // current tip
   currentTip;
 
-
-  // select Tip
+  // select tip
   selectTip(){
-    this.currentTip = this.tips[this.currentTipIndex];
-    // call the update display method...
-    this.updateDisplay();
+    // get the current tip
+    this.currentTip  =  this.tips[this.currentTipIndex];
 
-    // increment the value of the current Tip Index
-    this.currentTipIndex ++;
 
-    // ensure that the  current Tip index doesnt go out of range
-    if (this.currentTipIndex > this.tips.length - 1) {this.currentTipIndex = 0; }
-  }
+    // update the display (pass the current index )
+    this.updateDisplay(this.currentTip, this.currentTipIndex);
 
-  // update Display
-  updateDisplay(){
-    // letter index
-    let letterIndex = 0;
-    // write the tip letter by letter (timer function)
-    this.currentTip.split().forEach((tip) => {
-      // append the  current letter to the the screen
-      document.querySelector('[data-displayScreen]').innerHTML += tip[letterIndex];
-    });
+    // increment the value of the tipIndex
+    this.currentTipIndex++;
 
+    // range check (return current tip index to zero if it exceeds limit)
+    if(this.currentTipIndex>this.tips.length - 1) this.currentTipIndex = 0;
 
   }
 
+
+  // update the display
+  updateDisplay(tip, tipIndex){
+
+    // find the container or element who's value is equal to the tip index and put  the word in that one
+
+
+
+    // define the current index of the character being displayed
+    let characterIndex = 0 ;
+    // define word to be what is being displayed
+    let word = '';
+
+
+    // set interval for attaching / displaying the word
+    const attachCharacter = setInterval(() => {
+      word += tip[characterIndex];
+      // display the word
+      document.querySelector('[data-testOutput]').innerHTML = word ;
+      // increment the value of the characterIndex
+      characterIndex ++ ;
+      // range check
+      if (characterIndex > tip.length - 1){
+        //  clear the interval
+          clearInterval(attachCharacter);
+          characterIndex = 0;
+        }
+    } , 100 );
+
+
+
+  }
 
   constructor() { }
 
   ngOnInit(): void {
-    setInterval(() => {
-      this.selectTip();
-    }, 2000);
+    // select the tip
+
+    // set an interval
+    setInterval(() => this.selectTip(),7000)
+
     this.selectTip();
   }
+
+  ngAfterViewInit(): void {
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+
+  }
+
+
+
 
 }
---
+++ login/src/app/tipsAndRulesInterface.ts
@@ -0,0 +1,6 @@
+import { Interface } from "readline";
+
+export interface TipsAndRules{
+  'htmlReference': number;
+  'content': string;
+};
